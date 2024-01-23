d17b5d17ff527c72b5c06d58221afb315c45d789
shavidissa <shavindri.dissanayake@gmail.com>
2024-01-23 15:49:43 -0800

    number exercises

---
+++ number_assignment.py
@@ -0,0 +1,16 @@
+# Excersice 2.9
+#Write addition, subtraction, multiplication, and division operations that each result in the number 8. 
+# Be sure to enclose your operations in print() calls to see the results. 
+
+print (5+3)
+print(4.4+3.6)
+print(8/1)
+print(4*2)
+
+# Exerceis 2.10. 
+#Favorite Number: Use a variable to represent your favorite number.
+# Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+FAVORITE_NUMBER = 4.67888 + 2.56437
+message = f"My favourite number is: {FAVORITE_NUMBER}"
+print(message)
