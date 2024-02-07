776cec3020d63740c21521681dfdbb41aae3089d
shavidissa <shavindri.dissanayake@gmail.com>
2024-02-07 15:42:24 -0800

    learn list

---
+++ list.py
@@ -0,0 +1,17 @@
+my_list = ["Bicycle", "car", "van"]
+print(my_list)
+
+#Print the first item on the list.
+print(my_list[0])
+
+#String methods work on lists that include strings.
+print(my_list[0].lower())
+
+#Get the last item on the list.
+print(my_list[-1])
+print(my_list[-2])
+
+#Using individual values from a list.
+message = f"my first vehicle was a {my_list[1]}."
+print(message)
+
